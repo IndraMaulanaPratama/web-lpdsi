abeb9751c1f032a0e273d9fe8fbff4abe453292d
IndraMaulanaPratama <maulana.indra808@gmail.com>
2025-12-16 22:19:19 +0700

    Fixing Bug || target view not found on Layanan Controller

--- app/Http/Controllers/LayananController.php
+++ app/Http/Controllers/LayananController.php
@@ -15,24 +15,24 @@ class LayananController extends Controller
          // Kalau hanya 1 data yang aktif ditampilkan:
         $lab = LabKomputer::latest()->first();
 
-        return view('layanan.komputer', compact('lab'));
+        return view('Layanan.komputer', compact('lab'));
     }
 
     public function bahasa()
     {
         $lab = LabBahasa::latest()->first();
-        return view('layanan.bahasa', compact('lab'));
+        return view('Layanan.bahasa', compact('lab'));
     }
 
     public function pemerintahan()
     {
         $lab = LabPemerintahan::latest()->first();
-        return view('layanan.pemerintahan', compact('lab'));
+        return view('Layanan.pemerintahan', compact('lab'));
     }
 
     public function pddikti()
     {
         $lab = Pddikti::latest()->first();
-        return view('layanan.pddikti', compact('lab'));
+        return view('Layanan.pddikti', compact('lab'));
     }
 }
